5217938cab65ad413bf461405fa4bac585b2f91a
Duong Thai Tran <duongthaitran68@gmail.com>
2024-03-08 20:28:15 +0700

    guessIt 2

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -83,7 +83,7 @@ bool checkSuccess(string answer) {
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
     bool result = false;
-    if(isContinued == 'Y'){
+    if(isContinued == 'Y' || isContinued == 'y'){
         result = true;
     }
     return result;
