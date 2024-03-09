e9313a69946e425bc22379cf3d52c28882a82376
Dainn98 <tuananhk56a1@gmail.com>
2024-03-09 09:36:04 +0700

    New Hello Hello

--- AdvProg_L0-Hello/hello.cpp
+++ AdvProg_L0-Hello/hello.cpp
@@ -4,6 +4,7 @@
 std::string printGameOver(){
 	// TODO: Return the required string
 	//hello world
+	//hello world/hello world/hello world/hello world/hello world/hello world/hello world/hello world
 	return "Game Over!!!!!\nRevive";
 	//12345789
 
