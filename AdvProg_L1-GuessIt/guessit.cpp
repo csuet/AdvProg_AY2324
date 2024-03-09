aaccd6c2387f71cbbef9e966bd3b44e96f27921c
Nguyen Viet Lam <nguyenlam25022005@gmail.com>
2024-03-09 22:05:50 +0700

    guessit edited 4

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -84,7 +84,7 @@ bool checkSuccess(string answer) {
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
     bool result = false;
-    if (isContinued == 'Y') result = true;
+    if (isContinued == 'Y' || isContinued == 'y') result = true;
     return result;
 }
 
