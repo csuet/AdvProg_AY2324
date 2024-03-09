dfa2420014145c68cf4f8ffc5fb8937ef4ff52b1
Nguyen Viet Lam <nguyenlam25022005@gmail.com>
2024-03-09 21:53:29 +0700

    guessit edited

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -13,8 +13,9 @@ using namespace std;
         number (int) : random number in range 1-100
 ***/
 int generateRandomNumber() {
+    int randomNumber = rand() % 100 + 1;
     // TODO: Return the random number in range 1 to 100
-    return 100;
+    return randomNumber;
 }
 
 
@@ -26,8 +27,10 @@ int generateRandomNumber() {
 ***/
 int getPlayerGuess() {
     // TODO: Ask the player guest and return the player's number
-
-    return 1;
+    cout << "Enter your number: ";
+    int guessNumber;
+    cin >> guessNumber;
+    return guessNumber;
 }
 
 
@@ -46,7 +49,15 @@ string getAnswer(int number, int randomNumber) {
               If number is equal randomNumber, the answer is "Congratulation! You win."
     ***/
     string answer;
-
+    if (number == randomNumber){
+        answer = "Congratulation! You win.";   
+    }
+    else if (number < randomNumber){
+        answer = "Your number is lower.";
+    }
+    else {
+        answer = "Your number is higher.";
+    }
     return answer;
 }
 
@@ -59,8 +70,9 @@ string getAnswer(int number, int randomNumber) {
 ***/
 bool checkSuccess(string answer) {
     // TODO: return the result after checking that player guessed right or wrong
-    
-    return true;
+    if (answer == "Congratulation! You win.")
+        return true;
+    return false;
 }
 
 
@@ -73,7 +85,7 @@ bool checkSuccess(string answer) {
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
     bool result = false;
-
+    if (isContinued == 'y') result = true;
     return result;
 }
 
@@ -87,7 +99,8 @@ bool checkContinuePlaying(char isContinued) {
 char getPlayerOpinion() {
     // TODO: Ask the player about continue playing and return the player's choice
     char isContinued;
-
+    cout << "Do you want to play again? (y/n)";
+    cin >> isContinued;
     return isContinued;
 }
 
