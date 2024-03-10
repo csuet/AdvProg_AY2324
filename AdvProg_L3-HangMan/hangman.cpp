0387cdd1a75ea541f4bb97feb4e677e82bbc6991
dangdd2208 <dinhdang2208@gmail.com>
2024-03-10 20:38:17 +0700

    hi

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -13,7 +13,7 @@ int generateRandomNumber(const int min, const int max)
 {
     // TODO: Return a random integer number between min and max
     srand(time(NULL));
-    int n = rand() % (max - min + 1) + min;
+    int n = rand() % (min-max) + min +1;
     return n;
 }
 
