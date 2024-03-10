1c517dfe9f7ef51bdae998b8ae91bd90179772f0
dangdd2208 <dinhdang2208@gmail.com>
2024-03-10 19:55:16 +0700

    hi

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -1,4 +1,12 @@
-#include <iostream>
+
+
+User
+hoàn thành các hàm sau
+ChatGPT
+Rất vui được giúp bạn hoàn thành các hàm. Tuy nhiên, bạn chưa cung cấp danh sách các hàm cần hoàn thành. Bạn có thể liệt kê các hàm cần hoàn thành cùng yêu cầu cụ thể cho mỗi hàm được không? Sau đó, tôi sẽ cố gắng hỗ trợ bạn hoàn thành chúng.
+
+User
+hoàn thành các hàm sau#include <iostream>
 #include "hangman.h"
 
 using std::string;
@@ -182,3 +190,115 @@ void processData(const char ch, const string& word,
     }
 }
 
+ChatGPT
+Dưới đây là các hàm đã được hoàn thành dựa trên yêu cầu của bạn:
+
+cpp
+Copy code
+#include <iostream>
+#include "hangman.h"
+#include <vector>
+#include <fstream>
+#include <stdexcept>
+#include <ctime>
+
+using std::string;
+using std::vector;
+using std::ifstream;
+using std::domain_error;
+using std::cin;
+using std::tolower;
+
+int generateRandomNumber(const int min, const int max)
+{
+    srand(time(NULL));
+    int n = rand() % (max - min + 1) + min;
+    return n;
+}
+
+vector<string> readWordListFromFile(const string& filePath)
+{
+    vector<string> wordList;
+    string word;
+    ifstream wordFile(filePath);
+    if (!wordFile.is_open()) {
+        throw domain_error("Unable to open file");
+    }
+
+    while (wordFile >> word) {
+        wordList.push_back(word);
+    }
+    wordFile.close();
+
+    return wordList;
+}
+
+bool isCharInWord(const char ch, const string& word)
+{
+    for (char c : word) {
+        if (tolower(c) == tolower(ch))
+            return true;
+    }
+    return false;
+}
+
+string chooseWordFromList(const vector<string>& wordList, int index)
+{
+    string answer;
+    if (index >= 0 && index < wordList.size())
+        answer = wordList[index];
+    else
+        answer = "invalid";
+    return answer;
+}
+
+string generateHiddenCharacters(string answerWord)
+{
+    string secretWord;
+    for (char c : answerWord) {
+        if (isalpha(c)) {
+            secretWord += '-';
+        } else {
+            secretWord += c;
+        }
+    }
+    return secretWord;
+}
+
+char getInputCharacter() {
+    char ch;
+    cin >> ch;
+    return tolower(ch); 
+}
+
+void updateSecretWord(string& secretWord, const char ch, const string& word)
+{
+    for (int i = 0; i < word.size(); i++) {
+        if (tolower(ch) == tolower(word[i]))
+            secretWord[i] = word[i];
+    }
+}
+
+void updateEnteredChars(const char ch, string& chars)
+{
+    chars += ch;
+}
+
+void updateIncorrectGuess(int& incorrectGuess)
+{
+    incorrectGuess++;
+}
+
+void processData(const char ch, const string& word, 
+                string& secretWord, 
+                string& correctChars, 
+                int& incorrectGuess, string& incorrectChars)
+{
+    if (isCharInWord(ch, word)) {
+        updateSecretWord(secretWord, ch, word);
+        updateEnteredChars(ch, correctChars);
+    } else {
+        updateIncorrectGuess(incorrectGuess);
+        updateEnteredChars(ch, incorrectChars);
+    }
+}
