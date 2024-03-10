6879f9391905555c0968f3db7e0d821c9a17d604
congkx123789 <94501695+congkx123789@users.noreply.github.com>
2024-03-10 22:32:16 +0700

    calculus.cpp

--- AdvProg_L2-Calculus/calculus.cpp
+++ AdvProg_L2-Calculus/calculus.cpp
@@ -2,11 +2,10 @@
 #include <string>
 #include <cmath>
 
-using std::string;
 using std::cout;
 using std::endl;
 using std::stod;
-
+using std::string;
 
 double mySin(double x);
 double myCos(double x);
@@ -18,9 +17,11 @@ double mySqrt(double x);
     Returns:
         double: cosine of x
 ***/
-double myCos(double x) 
+double myCos(double x)
 {
-    return 0.0;
+    int a;
+    double ans = 1 - (1 / 2) * pow(x, 2) + (1 / 24) * pow(x, 4) - (1 / 720) * pow(x, 6);
+    return ans;
 }
 
 /***
@@ -31,9 +32,9 @@ double myCos(double x)
 ***/
 double mySin(double x)
 {
-    return 0.0;
+    double ans = x - (1 / 6) * pow(x, 3) + (1 / 120) * pow(x, 5) - (1 / 5040) * pow(x, 7);
+    return ans;
 }
-
 
 /***
     Args:
@@ -41,12 +42,19 @@ double mySin(double x)
     Returns:
         double: square root of x
 ***/
-double mySqrt(double x) {
-    if (x < 0) {
+double mySqrt(double x)
+{
+    if (x < 0)
+    {
         cout << "Invalid argument" << endl;
         exit(1);
     }
+    double ans = 10;
+    for (int i = 0; i < 6; i++)
+    {
 
-    
-    return 0;
+        ans = ans - ((pow(ans, 2) - x) / (2 * ans));
+    }
+
+    return ans;
 }
