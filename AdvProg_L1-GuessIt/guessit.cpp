2d4c0e2013cf8ca1fee099adab7da4e38bfc3306
NVCuong <cuong19012005@gmail.com>
2024-03-10 09:54:01 +0700

    resubmit guessit

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -79,9 +79,7 @@ bool checkSuccess(string answer) {
 ***/
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
-    bool result = false;
-    if (isContinued == 'Y') result = true;
-    return result;
+    return isContinued == 'Y';
 }
 
 
