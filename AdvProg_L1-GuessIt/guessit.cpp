957262f693e7eeb02ba10f9f4fc5242fafc2280e
Huy1902 <lymstar19@gmail.com>
2024-03-11 19:51:40 +0700

    Update

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -71,7 +71,7 @@ string getAnswer(int number, int randomNumber) {
 ***/
 bool checkSuccess(string answer) {
     // TODO: return the result after checking that player guessed right or wrong
-    if (answer == "Congratulation! You win.")
+    if (answer != "Congratulation! You win.")
     {
         return false;
     }
