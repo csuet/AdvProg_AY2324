7b3885d957ff211ba473cb3e0bcdc7004b2136c4
dinhuet <23020043@vnu.edu.vn>
2024-03-11 22:38:35 +0700

    hangman

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -131,6 +131,7 @@ void updateSecretWord(string& secretWord, const char ch, const string& word)
 ***/
 void updateEnteredChars(const char ch, string& chars){
     // TODO: append the character ch is in end of the text chars
+    chars += " ";
     chars.push_back(ch);
 }
 
