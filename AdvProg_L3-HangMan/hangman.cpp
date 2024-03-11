c1535d29a2b678346c04de4517345fcada83d19a
dinhuet <23020043@vnu.edu.vn>
2024-03-11 22:33:21 +0700

    hangman

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -73,7 +73,7 @@ string chooseWordFromList(const vector<string>& wordList, int index)
     // TODO: Return a lowercase word in the index position of the vector wordList.
     string answer;
     answer = wordList[index];
-    for(int i = 0; i < answer; i++)
+    for(int i = 0; i < answer.size(); i++)
     {
         answer[i] = tolower(answer[i]);
     }
