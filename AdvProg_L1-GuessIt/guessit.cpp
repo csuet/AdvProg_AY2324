92ce3336d71d82ab54a207e33952475a2c8a055f
Hoanggiabao1 <hgiabao88@gmail.com>
2024-03-11 20:42:15 +0700

    GuessIt vs Calculus

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -47,9 +47,13 @@ string getAnswer(int number, int randomNumber) {
               If number is equal randomNumber, the answer is "Congratulation! You win."
     ***/
     string answer;
-    if (number > randomNumber){answer = "Your number is higher.";}
-    if (number < randomNumber){answer = "Your number is lower.";}
-    if (number > randomNumber){answer = "Congratulation! You win.";}
+    if (number > randomNumber) {
+        answer = "Your number is higher.";
+    } else if (number < randomNumber) {
+        answer = "Your number is lower.";
+    } else {
+        answer = "Congratulations! You win.";
+    }
     return answer;
 }
 
