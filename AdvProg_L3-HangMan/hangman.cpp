dd1d35a15ca661bcc931f55fc13dc43744b26c9a
lequocanh111 <lequocanh14012005@gmail.com>
2024-03-11 14:46:42 +0700

    New Hangman

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -17,7 +17,8 @@ using std::cin;
 int generateRandomNumber(const int min, const int max)
 {
     // TODO: Return a random integer number between min and max
-    return 1;
+    int so = rand()%(max-min+1)+min;
+    return so;
 }
 
 vector<string> readWordListFromFile(const string& filePath)
@@ -51,7 +52,8 @@ vector<string> readWordListFromFile(const string& filePath)
 bool isCharInWord(const char ch, const string& word)
 {
     // TODO: return true if ch is in word else return false
-    return true;
+    for(int i=0;i<word.size();i++) if(ch==word[i]) return true;
+    return false;
 }
 
 /***
@@ -65,7 +67,7 @@ string chooseWordFromList(const vector<string>& wordList, int index)
 {
     // TODO: Return a lowercase word in the index position of the vector wordList.
     string answer;
-
+    answer = wordList[index];
     return answer;
 }
 
@@ -99,6 +101,7 @@ char getInputCharacter() {
 void updateSecretWord(string& secretWord, const char ch, const string& word)
 {
     // TODO: Update the secret word if the character ch is in the answer word.
+    for(int i=0;i<word;i++) if(ch==word[i]) {sercetWord[i]=word[i];}
 }
 
 /***
@@ -110,6 +113,8 @@ void updateSecretWord(string& secretWord, const char ch, const string& word)
 ***/
 void updateEnteredChars(const char ch, string& chars){
     // TODO: append the character ch is in end of the text chars
+    chars = chars + ch + " ";
+    return chars;
 }
 
 /***
@@ -120,6 +125,8 @@ void updateEnteredChars(const char ch, string& chars){
 ***/
 void updateIncorrectGuess(int& incorrectGuess){
     // TODO: increase the value of incorrectGuess by 1
+    incorrectGuess = incorrectGuess + 1;
+    return incorrectGuess;
 }
 
 /***
@@ -146,5 +153,7 @@ void processData(const char ch, const string& word,
             update incorrectGuess: call updateIncorrectGuess() function
             update incorrectChars: call updateEnteredChars() function
     ***/
+    for(int i=0;i<word.size();i++) if(ch==word[i]){updateSercetWord();updateEnteredChars();return;}
+    updateIncorrectGuess();updateEnteredChars();return;
 }
 
