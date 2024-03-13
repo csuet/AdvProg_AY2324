a0d907bc7f5dc040922d18b03f1d9942803b9712
luupin141 <ducluutk@gmail.com>
2024-03-13 11:34:11 +0700

    new

--- AdvProg_L2-Calculus/calculus.cpp
+++ AdvProg_L2-Calculus/calculus.cpp
@@ -5,7 +5,7 @@
 using std::string;
 using std::cout;
 using std::endl;
-using std::stod;
+
 
 
 double mySin(double x);
@@ -18,9 +18,21 @@ double mySqrt(double x);
     Returns:
         double: cosine of x
 ***/
-double myCos(double x) 
+double myCos(double x)
 {
-    return 0.0;
+    const double epsilon = 1e-15;
+    double result = 1.0;
+    double term = x;
+    int sign = -1;
+    for (int i = 2; i <= 100; i += 2) {
+        term *= -x * x / (i * (i - 1));
+        result += sign * term;
+        sign = -sign;
+        if (std::abs(term) < epsilon) {
+            break;
+        }
+    }
+    return result;
 }
 
 /***
@@ -31,7 +43,7 @@ double myCos(double x)
 ***/
 double mySin(double x)
 {
-    return 0.0;
+    return myCos(x - M_PI / 2);
 }
 
 
@@ -46,7 +58,18 @@ double mySqrt(double x) {
         cout << "Invalid argument" << endl;
         exit(1);
     }
+    double low = 0;
+    double high = x;
+    double mid;
 
-    
-    return 0;
+    while (high - low > 1e-15) {
+        mid = (low + high) / 2;
+        if (mid * mid > x) {
+            high = mid;
+        } else {
+            low = mid;
+        }
+    }
+
+    return low;
 }
