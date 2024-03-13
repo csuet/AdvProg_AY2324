fd1be49413d33e16d71e43379f36ef255fa06ddc
Ta Duy Khanh <khanhtaduy2k5@gmail.com>
2024-03-13 20:10:20 +0700

    lec1-guessit1.0

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -50,7 +50,7 @@ string getAnswer(int number, int randomNumber) {
     string answer;
     if (number > randomNumber) answer = "Your number is higher.";
     else if (number < randomNumber) answer = "Your number is lower.";
-    else answer = "Congratulation! You win";
+    else answer = "Congratulation! You win.";
     return answer;
 }
 
