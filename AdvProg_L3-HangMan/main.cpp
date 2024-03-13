5ce9a076bc3a2e4ff87321ba97fdbccddfc680b7
tunglamp08072005 <tunglamp227@gmail.com>
2024-03-14 00:12:11 +0700

    update

--- AdvProg_L3-HangMan/main.cpp
+++ AdvProg_L3-HangMan/main.cpp
@@ -12,45 +12,57 @@ int main()
 {
     srand((int)time(0));
 
-    string vocabularyFile = "data/Ogden_Picturable_200.txt";
+    string vocabularyFile = "Ogden_Picturable_200.txt";
     //string vocabularyFile = "data/ErrorOpenFileTest.txt";
     //string vocabularyFile = "data/EmptyTest.txt";
     vector<string> wordList;
-    try {
-        wordList = readWordListFromFile(vocabularyFile);
-    } catch (domain_error) {
-        cout << endl << "Error: in reading vocabulary file: " << vocabularyFile << endl;
-        return 1;
-    }
 
-    int index = generateRandomNumber(0, wordList.size()-1);
-    string word = chooseWordFromList(wordList, index);
+    do{
+        try{
+            wordList = readWordListFromFile(vocabularyFile);
+        } catch(std::exception& e){
+            cout << endl << "Loi: " << e.what() << " trong doc tu trong: " << vocabularyFile << endl;
+            return 1;
+        }
 
-    if (word.empty()) {
-        cout << "Error: Coud not choose a random word." << endl;
-        return 1;
-    }
-    //cout << "Chosen word: " << word << endl;
-    // string secretWord(word.length(), '-');
-    string secretWord = generateHiddenCharacters(word);
+        int index = generateRandomNumber(0, wordList.size() - 1);
+        string word = chooseWordFromList(wordList, index);
 
-    int incorrectGuess = 0;
-    string correctChars = "";
-    string incorrectChars = "";
-    
-    printScreen(word, secretWord, correctChars, incorrectGuess, incorrectChars); 
-    
-    do {
-        char ch = getInputCharacter();
+        if(word.empty())
+        {
+            cout << "Loi: khong the chon tu bat ky" << endl;
+            return 1;
+        }
 
-        processData(ch, word, secretWord, 
-                    correctChars, incorrectGuess, incorrectChars);
-        
-        printScreen(word, secretWord, correctChars, incorrectGuess, incorrectChars); 
+        string secretWord = generateHiddenCharacters(word);
+        int incorrectGuess = 0;
+        string correctChars = "";
+        string incorrectChars = "";
 
-    } while (secretWord != word && incorrectGuess != MAX_MISTAKES-1);
+        do{
+            char ch = getInputCharacter();
+            processData(ch, word, secretWord, correctChars, incorrectGuess, incorrectChars);
 
-    playAnimation(word, secretWord, correctChars, incorrectGuess, incorrectChars);
+            printScreen(word, secretWord, correctChars, incorrectGuess, incorrectChars);
 
+            // Kiem tra trang thai cua tro choi (doan dung het hoac doan sai qua so lan cho phep)
+            if(secretWord == word || incorrectGuess >= MAX_MISTAKES)
+            {
+                playAnimation(word, secretWord, correctChars, incorrectGuess, incorrectChars);
+                break; // Ket thuc vong lap khi tro choi ket thuc
+            }
+        } while(true); // Vong lap vo han
+
+        // Hien thi diem so va xac dinh xem nguoi choi muon choi tiep hay khong
+        cout << "Ban co muon choi lai khong? (y/n): ";
+        char choiLai;
+        cin >> choiLai;
+
+        if(tolower(choiLai) != 'y'){
+            cout << "Game over!" << endl;
+            break;
+        }
+
+    } while(true);
     return 0;
 }
