d1c6fb8a0c1d5a9ba77195ab8f7cf9b81a78109c
le1510 <23020695@vnu.edu.vn>
2024-03-13 20:44:57 +0700

    update

--- AdvProg_L2-Calculus/calculus.cpp
+++ AdvProg_L2-Calculus/calculus.cpp
@@ -20,16 +20,8 @@ double mySqrt(double x);
 ***/
 double myCos(double x) 
 {
-	
-double result = 0.0;
-    double term = 1;
-    int sign = 1;
-    for (int i = 0; i <= 10; ++i) { // Number of terms in the series (adjust as needed)
-        result += sign * term;
-        term *= -1 * x * x / ((2 * i + 1) * (2 * i + 2));
-        sign *= -1;
-    }
-    return result;}
+    return 0.0;
+}
 
 /***
     Args:
@@ -39,15 +31,8 @@ double result = 0.0;
 ***/
 double mySin(double x)
 {
-double result = 0.0;
-    double term = x;
-    int sign = 1;
-    for (int i = 1; i <= 10; ++i) { 
-        result += sign * term;
-        term *= -1 * x * x / ((2 * i) * (2 * i + 1));
-        sign *= -1;
-    }
-    return result;}
+    return 0.0;
+}
 
 
 /***
@@ -62,10 +47,6 @@ double mySqrt(double x) {
         exit(1);
     }
 
-    double guess = x / 2; // Initial guess (can be arbitrary)
-    double threshold = 1e-6; // Threshold for convergence (adjust as needed)
-    while (abs(guess * guess - x) > threshold) {
-        guess = (guess + x / guess) / 2;
-    }
-    return guess;
+    
+    return 0;
 }
