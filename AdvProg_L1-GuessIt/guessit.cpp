03ece98d196cce8d6cf72f6e062004a6fb2617d4
le1510 <23020695@vnu.edu.vn>
2024-03-13 19:17:05 +0700

    update

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -14,7 +14,7 @@ using namespace std;
 ***/
 int generateRandomNumber() {
     // TODO: Return the random number in range 1 to 100
-    return 100;
+    return rand() % 100 + 1;
 }
 
 
@@ -27,8 +27,9 @@ int generateRandomNumber() {
 int getPlayerGuess() {
     // TODO: Ask the player guest and return the player's number
 
-    return 1;
-}
+int guess;
+    cin >> guess;
+    return guess;}
 
 
 /***
@@ -46,7 +47,13 @@ string getAnswer(int number, int randomNumber) {
               If number is equal randomNumber, the answer is "Congratulation! You win."
     ***/
     string answer;
-
+    if (number > randomNumber)
+        answer = "Your number is higher.";
+    else if (number < randomNumber)
+        answer = "Your number is lower.";
+    else
+        answer = "Congratulations! You win.";
+    return answer;
     return answer;
 }
 
@@ -59,8 +66,11 @@ string getAnswer(int number, int randomNumber) {
 ***/
 bool checkSuccess(string answer) {
     // TODO: return the result after checking that player guessed right or wrong
-    
-    return true;
+    if(answer == "Congratulations! You win.")
+    {
+	return true;
+	
+}
 }
 
 
@@ -73,7 +83,10 @@ bool checkSuccess(string answer) {
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
     bool result = false;
-
+    if(isContinued == 'y' || isContinued == 'Y')
+    {
+    	result=true;
+	}
     return result;
 }
 
@@ -87,7 +100,7 @@ bool checkContinuePlaying(char isContinued) {
 char getPlayerOpinion() {
     // TODO: Ask the player about continue playing and return the player's choice
     char isContinued;
-
+    cin>>isContinued;
     return isContinued;
 }
 
