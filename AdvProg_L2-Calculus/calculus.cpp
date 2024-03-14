e4d4650b3ea5a6e3e60b2a3d265e56dc0dc6f781
tn26205 <160998770+tn26205@users.noreply.github.com>
2024-03-14 22:58:08 +0700

    Update calculus.cpp

--- AdvProg_L2-Calculus/calculus.cpp
+++ AdvProg_L2-Calculus/calculus.cpp
@@ -26,7 +26,7 @@ double myCos(double x)
         result += temp;
         n++;
         temp *= -x*x/((2*n-1)*2*n);
-    }while( abs(temp) >0.001);
+    }while( abs(temp) >0.000000001);
     return result;
 }
 
@@ -44,7 +44,7 @@ double mySin(double x)
         result += temp;
         n++;
         temp *= -x*x/(2*n*(2*n+1));
-    }while (abs (temp) > 0.001);
+    }while (abs (temp) > 0.000000001);
     return result;
 }
 
@@ -64,7 +64,7 @@ double mySqrt(double x) {
     
     else{
         double temp = 0.0, a = 1.0;
-        while( abs(temp*temp - x) > 0.001 ){
+        while( abs(temp*temp - x) > 0.000000001 ){
             temp = 0.5*(a+x/a);
             a=temp;
         }
