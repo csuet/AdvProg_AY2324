ccc7f0ace92dfde17827412081b8e67bfac0504d
bthdz <hieubui442005@gmail.com>
2024-03-14 11:31:36 +0700

    hello.cpp

--- AdvProg_L0-Hello/hello.cpp
+++ AdvProg_L0-Hello/hello.cpp
@@ -2,10 +2,6 @@
 #include "hello.h"
 std::string printGameOver(){
 	// TODO: Return the required string
-<<<<<<< HEAD
 	string a = "Game over!";
 	return a;
-=======
-	return "Game over!!!!";
->>>>>>> 5de6060092ef3ffa37eb8789b2dbbc3b458990f5
 }
