94960300bbbb0eee31d6b6cd6f36420e3338b67d
bthdz <hieubui442005@gmail.com>
2024-03-14 12:07:10 +0700

    guessit.cpp

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -76,7 +76,7 @@ bool checkSuccess(string answer) {
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
     bool result = false;
-	if(isContinued != ' ') result = true;
+	if(isContinued == '1') result = true;
     return result;
 }
 
