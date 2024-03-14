a5368285edd94e29ab1084863d093f788afe9a74
trinhxuanhoa <bongnaiton30@gmail.com>
2024-03-14 10:13:17 +0700

    (new caculator)

--- AdvProg_L2-Calculus/calculus.cpp
+++ AdvProg_L2-Calculus/calculus.cpp
@@ -20,7 +20,19 @@ double mySqrt(double x);
 ***/
 double myCos(double x) 
 {
-    return 0.0;
+    
+     x = x * M_PI / 180.0;
+    double result = x;
+    double term = x;
+    double sign = -1.0;
+
+    for (int i = 1; i < 10; ++i) {
+        term *= x * x / ((2 * i) * (2 * i + 1));
+        result += sign * term;
+        sign *= -1.0;
+    }
+
+    return result;
 }
 
 /***
@@ -31,7 +43,18 @@ double myCos(double x)
 ***/
 double mySin(double x)
 {
-    return 0.0;
+    x = x * M_PI / 180.0;
+    double result = 1.0;
+    double term = 1.0;
+    double sign = -1.0;
+
+    for (int i = 1; i < 10; ++i) {
+        term *= x * x / ((2 * i - 1) * (2 * i));
+        result += sign * term;
+        sign *= -1.0;
+    }
+
+    return result;
 }
 
 
@@ -42,11 +65,16 @@ double mySin(double x)
         double: square root of x
 ***/
 double mySqrt(double x) {
-    if (x < 0) {
-        cout << "Invalid argument" << endl;
-        exit(1);
+   
+    double guess = x / 2.0; // Ước đoán ban đầu
+    double oldGuess = 0.0;
+
+    while (abs(guess - oldGuess) > 1e-6) {
+        oldGuess = guess;
+        guess = 0.5 * (guess + x / guess); // Lặp Newton-Raphson
     }
 
+    return guess;
+
     
-    return 0;
 }
