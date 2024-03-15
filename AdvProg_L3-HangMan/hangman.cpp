f73b3d8ea433a77a3dc8176c9e6f687ef6a9d023
annhldh <162427826+annhldh@users.noreply.github.com>
2024-03-15 17:56:32 +0700

    Update hangman.cpp

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -92,7 +92,7 @@ string chooseWordFromList(const vector<string>& wordList, int index)
 string generateHiddenCharacters(string answerWord){
     // TODO: Based on answerWord's length, generate hidden characters in form of "---"
     string secretWord;
-    for (char x: answerWord)secretWord+='_';
+    for (char x: answerWord)secretWord+='-';
     return secretWord;
 }
 
