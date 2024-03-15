28792324dac584ce09976ff4b16aaff2690d9859
Tanh-Ng <annhnguyen0412@gmail.com>
2024-03-15 14:53:20 +0700

    hang man

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -164,7 +164,7 @@ void processData(const char ch, const string& word,
     ***/
    if(isCharInWord(ch,word)){
     updateSecretWord(secretWord,ch,word);
-    updateEnteredChars(ch,incorrectChars);
+    updateEnteredChars(ch,correctChars);
    }
    else{
     updateIncorrectGuess(incorrectGuess);
