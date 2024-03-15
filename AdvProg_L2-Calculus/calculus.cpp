18a84f6aa81fc1b4eed8e5943621d62a63a93bb4
bacbahieu <letuancanhhd@gamil.com>
2024-03-15 10:32:58 +0700

    Bai_3_Calculus_7.0

--- AdvProg_L2-Calculus/calculus.cpp
+++ AdvProg_L2-Calculus/calculus.cpp
@@ -21,7 +21,7 @@ double myCos(double x)
 {
     double ket_qua = 0.0;
     int giai_thua = 1;
-    for (int i = 0; i < 20; i++)
+    for (int i = 0; i < 16; i++)
     {
         if (i == 0)
         {
@@ -51,7 +51,7 @@ double mySin(double x)
 {
     double ket_qua = 0.0;
     int giai_thua = 1;
-    for (int i = 0; i < 20; i++)
+    for (int i = 0; i < 16; i++)
     {
         if (i == 0)
         {
@@ -90,7 +90,7 @@ double mySqrt(double x)
     else
     {
         double ket_qua = x;
-        for (int i = 0; i < 20; i++)
+        for (int i = 0; i < 10; i++)
         {
             ket_qua = (ket_qua + x / ket_qua) / 2;
         }
