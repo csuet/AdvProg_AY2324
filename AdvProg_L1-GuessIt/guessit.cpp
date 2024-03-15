8adce91df16fd46e731d177c553bd695c9088c90
Nguyen Bao Long <longlam0401@gmail.com>
2024-03-15 18:54:04 +0700

    4

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -81,7 +81,7 @@ bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
     bool result = false;
 
-    if(isContinued == 'y') result = true;
+    if(isContinued == 'y' || isContinued == 'Y') result = true;
 
     return result;
 }
