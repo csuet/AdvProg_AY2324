6776d8785ce817982bcf250638529bde13c8b46a
Ducdung005bn <ducdung005bn@gmail.com>
2024-03-15 13:31:07 +0700

    update calculus

--- AdvProg_L2-Calculus/calculus.cpp
+++ AdvProg_L2-Calculus/calculus.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+#include <math.h>
 
 using std::string;
 using std::cout;
 using std::endl;
-using std::stod;
+
 
 
 double mySin(double x);
@@ -18,9 +19,9 @@ double mySqrt(double x);
     Returns:
         double: cosine of x
 ***/
-double myCos(double x) 
+double myCos(double x)
 {
-    return 0.0;
+    return cos(x);
 }
 
 /***
@@ -31,7 +32,7 @@ double myCos(double x)
 ***/
 double mySin(double x)
 {
-    return 0.0;
+    return sin(x);
 }
 
 
@@ -47,6 +48,6 @@ double mySqrt(double x) {
         exit(1);
     }
 
-    
-    return 0;
+
+    return sqrt(x);
 }
