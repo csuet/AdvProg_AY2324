30cf765930b9a334e0673f6cb7fd6e225d069bcf
HoangNgocNhi <23020130@vnu.edu.vn>
2024-03-16 01:01:39 +0700

    update

--- AdvProg_L2-Calculus/calculus.cpp
+++ AdvProg_L2-Calculus/calculus.cpp
@@ -18,9 +18,18 @@ double mySqrt(double x);
     Returns:
         double: cosine of x
 ***/
-double myCos(double x) 
+double myCos(double x)
 {
-    return 0.0;
+    double sum=1, pso=1;
+    int i=1;
+    while( abs(pso) > 0.001 )
+    {
+        pso *= -(x*x)/(2*i*(2*i-1));
+        sum += pso;
+        i++;
+    }
+
+    return sum;
 }
 
 /***
@@ -31,7 +40,16 @@ double myCos(double x)
 ***/
 double mySin(double x)
 {
-    return 0.0;
+    double sum=x, pso=1;
+    int i=2;
+    while( abs(pso) > 0.001 )
+    {
+        pso *= -(x*x)/(2*i*(2*i+1));
+        sum += pso;
+        i++;
+    }
+
+    return sum;
 }
 
 
@@ -41,12 +59,35 @@ double mySin(double x)
     Returns:
         double: square root of x
 ***/
+
+double f( double x )
+{
+    return x*x-2;
+}
+
+double f_prime( double x )
+{
+    return 2*x;
+}
+
 double mySqrt(double x) {
     if (x < 0) {
         cout << "Invalid argument" << endl;
         exit(1);
     }
 
-    
+    double x0=1, x1, y, yprime;
+    while( abs(f_prime(x0)) > 0.001 )
+    {
+        y = f(x0);
+        yprime = f_prime(x0);
+        x1 = x0 - y/yprime;
+        if( abs(x1-x0) <= 0.001 ) return x1;
+
+        x0 = x1;
+    }
+
+    cout << "Invalid argument" << endl;
+    exit(1);
     return 0;
 }
