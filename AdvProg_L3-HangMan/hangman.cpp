2323f90900f56b39c8a0316e8dfff93ce17d5abf
NguyenHoaiNam2k5 <nam11220058a@gmail.com>
2024-03-15 21:23:10 +0700

    hangman2

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -137,6 +137,7 @@ void updateSecretWord(string& secretWord, const char ch, const string& word)
 ***/
 void updateEnteredChars(const char ch, string& chars){
     // TODO: append the character ch is in end of the text chars
+    chars += ' ';
     chars += ch;
 }
 
