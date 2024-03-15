74be51ff24ea677570a1eb8fc91d24c65d84e691
Hifumiii <hoangleminhminecraft@gmail.com>
2024-03-15 15:40:29 +0700

    fixed L2

--- AdvProg_L2-Calculus/calculus.cpp
+++ AdvProg_L2-Calculus/calculus.cpp
@@ -7,10 +7,18 @@ using std::cout;
 using std::endl;
 using std::stod;
 
-
+unsigned long long factorial(int n);
 double mySin(double x);
 double myCos(double x);
 double mySqrt(double x);
+
+unsigned long long factorial(int n) {
+    unsigned long long result = 1;
+    for (int i = 2; i <= n; ++i) {
+        result *= i;
+    }
+    return result;
+}
 
 /***
     Args:
@@ -20,7 +28,22 @@ double mySqrt(double x);
 ***/
 double myCos(double x) 
 {
-    return (1 - pow(x, 2)/2 + pow(x, 4)/24 - pow(x, 6)/720);
+    while (x > M_PI)
+        x -= M_PI * 2;
+
+    while (x < -M_PI)
+        x += M_PI * 2;
+
+    int i = 4;
+    double lastSequence = 1, newSequence = 1 - pow(x, 2)/2;
+    while (std::abs(lastSequence - newSequence) >= 0.001)
+    {
+        lastSequence = newSequence;
+        newSequence += pow(-1, i/2) * pow(x, i)/factorial(i);
+        i += 2;
+    }
+    
+    return newSequence;
 }
 
 /***
@@ -31,7 +54,22 @@ double myCos(double x)
 ***/
 double mySin(double x)
 {
-    return (x - pow(x, 3)/6 + pow(x, 5)/120 - pow(x, 7)/5040);
+    while (x > M_PI)
+        x -= M_PI * 2;
+
+    while (x < -M_PI)
+        x += M_PI * 2;
+
+    int i = 5;
+    double lastSequence = x, newSequence = x - pow(x, 3)/6;
+    while (std::abs(lastSequence - newSequence) >= 0.001)
+    {
+        lastSequence = newSequence;
+        newSequence += pow(-1, (i-1)/2) * pow(x, i)/factorial(i);
+        i += 2;
+    }
+    
+    return newSequence;
 }
 
 
