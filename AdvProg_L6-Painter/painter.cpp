e214641dca688d210c565dbbbf970b4befbb22a5
Hifumiii <hoangleminhminecraft@gmail.com>
2024-03-15 22:56:02 +0700

    mhm

--- AdvProg_L6-Painter/painter.cpp
+++ AdvProg_L6-Painter/painter.cpp
@@ -53,7 +53,7 @@ void Painter::jumpBackward(int numPixel)
 void Painter::turnLeft(double degree)
 {
     // TODO: rotate left the painter   
-    angle -= degree;
+    angle += degree;
 }
 
 
@@ -66,7 +66,7 @@ void Painter::turnLeft(double degree)
 void Painter::turnRight(double degree)
 {
     // TODO: rotate right the painter   
-    angle += degree;
+    angle -= degree;
 }
 
 /***  
