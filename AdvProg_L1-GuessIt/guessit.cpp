b4906f1aad7d854b8e3abde55e3e6c00b713ff37
Nguyen Tuan Khang <handlele08@gmail.com>
2024-03-15 10:32:19 +0700

    upp

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -29,7 +29,7 @@ int getPlayerGuess() {
 
     int number;
     cout << "The number you guess is (0 - 100): ";
-    cin >> number
+    cin >> number;
     return number;
 }
 
@@ -48,6 +48,7 @@ string getAnswer(int number, int randomNumber) {
               If number is lower than randomNumber, the answer is "Your number is lower."
               If number is equal randomNumber, the answer is "Congratulation! You win."
     ***/
+    int answer;
     if (number > randomNumber)
       {
       	answer = "Your number is higher";
@@ -109,7 +110,7 @@ bool checkContinuePlaying(char isContinued) {
 char getPlayerOpinion() {
     // TODO: Ask the player about continue playing and return the player's choice
     char isContinued;
-    cout << "Do you want to continue playing (Y/N): " 
+    cout << "Do you want to continue playing (Y/N): "; 
     cin >> is Continued;
     return isContinued;
 }
