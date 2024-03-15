5d019245ddd931019df84017685727d0162b0091
23020157tranthiphuongthao <161732192+23020157tranthiphuongthao@users.noreply.github.com>
2024-03-15 23:53:14 +0700

    Update guessit.cpp

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -50,7 +50,7 @@ string getAnswer(int number, int randomNumber) {
     string answer;
     if(number>randomNumber) answer="Your number is higher.";
     if(number<randomNumber) answer="Your number is lower.";
-    if(number==randomNumber) answer="Congratulation! You win."
+    if(number==randomNumber) answer="Congratulation! You win.";
     return answer;
 }
 
