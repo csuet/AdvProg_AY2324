6634ceb3c8a0ba7208afa4780dee77ed6d4b415f
DaoNangDiu <daonangdiu28102005@gmail.com>
2024-03-16 01:19:33 +0700

    hello

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -79,7 +79,7 @@ bool checkSuccess(string answer) {
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
     bool result = false;
-    if ( isContinued == 'y' || isContinued == "Y" ) result = true;
+    if ( isContinued == 'y' || isContinued == 'Y' ) result = true;
     return result;
 }
 
