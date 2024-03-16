69f20d61719069bb49de7c51ae53660d4b7ad6b7
HuyDang05 <quanghuy23052005@gmail.com>
2024-03-16 22:44:52 +0700

    push

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -136,7 +136,8 @@ void updateSecretWord(string& secretWord, const char ch, const string& word)
 ***/
 void updateEnteredChars(const char ch, string& chars){
     // TODO: append the character ch is in end of the text chars
-    chars + ch + ' ' ;
+    chars += ch;
+    chars += ' ';
 
 }
 
