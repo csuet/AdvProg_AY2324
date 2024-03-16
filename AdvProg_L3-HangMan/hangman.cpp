52a98834a77a042d2eafd8bf6768328bcec37866
HuyDang05 <quanghuy23052005@gmail.com>
2024-03-16 22:21:04 +0700

    check

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -19,7 +19,7 @@ using std::cin;
 int generateRandomNumber(const int min, const int max)
 {
     // TODO: Return a random integer number between min and max
-    srand(time(0));
+    srand(time(nullptr));
 
     return rand()%(max - min + 1) + min ;
 }
