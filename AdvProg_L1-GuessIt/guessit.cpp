fcb062431754c5ff2b022d18cbbf60a67da65480
Reminicent <animefansubotaku@gmail.com>
2024-03-16 11:07:58 +0700

    edited

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -8,24 +8,28 @@ using namespace std;
 
 /***
     Args:
-        
+
     Returns:
         number (int) : random number in range 1-100
 ***/
 int generateRandomNumber() {
-    // TODO: Return the random number in range 1 to 100
+    int random_number = rand() % 100 + 1;
     return 100;
 }
 
 
 /***
     Args:
-        
+
     Returns:
         number (int) : the number that player guessed
 ***/
 int getPlayerGuess() {
     // TODO: Ask the player guest and return the player's number
+    int your_number;
+
+    cout<<"Let's guess this number";
+    cin>>your_number;
 
     return 1;
 }
@@ -39,14 +43,16 @@ int getPlayerGuess() {
         answer (string) : answer of computer after checking result
 ***/
 string getAnswer(int number, int randomNumber) {
-    /*** 
+    /***
         TODO: check number with randomNumber and return the result.
               If number is higher than randomNumber, the answer is "Your number is higher."
               If number is lower than randomNumber, the answer is "Your number is lower."
               If number is equal randomNumber, the answer is "Congratulation! You win."
     ***/
     string answer;
-
+    if(your_number>random_number)answer="Your number is higher.";
+    if(your_number<random_number)answer="Your number is lower.";
+    if(your_number==random_number)answer="Congratulation! You win.";
     return answer;
 }
 
@@ -59,7 +65,7 @@ string getAnswer(int number, int randomNumber) {
 ***/
 bool checkSuccess(string answer) {
     // TODO: return the result after checking that player guessed right or wrong
-    
+
     return true;
 }
 
@@ -80,14 +86,17 @@ bool checkContinuePlaying(char isContinued) {
 
 /***
     Args:
-        
+
     Returns:
         isContinues (char) : player's choice (continue playing or not)
 ***/
 char getPlayerOpinion() {
     // TODO: Ask the player about continue playing and return the player's choice
     char isContinued;
-
+    char player_choice;
+    cout<<"Do you want to continue (Y/N)";
+    cin>>player_choice;
+    if (player_choice=='Y')isContinued='1'; else isContinued='0';
     return isContinued;
 }
 
@@ -96,7 +105,7 @@ void playGuessIt() {
     int randomNumber = generateRandomNumber();
     int number;
     string answer;
-    
+
     do {
         number = getPlayerGuess();
         answer = getAnswer(number, randomNumber);
