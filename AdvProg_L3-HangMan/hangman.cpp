dde0009a20d1a5bef418aff01de8dd2ae0b63b08
daohuyhoangggg <23020666@vnu.edu.vn>
2024-03-16 14:31:19 +0700

    update

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -17,7 +17,7 @@ using std::cin;
 int generateRandomNumber(const int min, const int max)
 {
     // TODO: Return a random integer number between min and max
-    return 1;
+    return rand()% max + min;
 }
 
 vector<string> readWordListFromFile(const string& filePath)
@@ -29,7 +29,7 @@ vector<string> readWordListFromFile(const string& filePath)
         throw domain_error("Unable to open file");
     }
 
-    //while ( getline (wordFile, word) ){  // Thong thuong doc tung line. 
+    //while ( getline (wordFile, word) ){  // Thong thuong doc tung line.
                                            // Chuong trinh nay cung chay.
     while (wordFile >> word) {  // Nhung voi chuong trinh nay, doc tung word cung duoc
                                 // Tuc ca 2 cach doc deu chay.
@@ -51,6 +51,8 @@ vector<string> readWordListFromFile(const string& filePath)
 bool isCharInWord(const char ch, const string& word)
 {
     // TODO: return true if ch is in word else return false
+    string temp = ""; temp += ch;
+    if(word.find(temp) == string::npos)return false;
     return true;
 }
 
@@ -61,11 +63,12 @@ bool isCharInWord(const char ch, const string& word)
     Returns:
         answer (string) : the lowercase word is in the position index of wordList
 ***/
-string chooseWordFromList(const vector<string>& wordList, int index) 
+string chooseWordFromList(const vector<string>& wordList, int index)
 {
     // TODO: Return a lowercase word in the index position of the vector wordList.
     string answer;
-
+    answer = wordList[index];
+    for(int i = 0; i < answer.size(); i++)answer[i] = tolower(answer[i]);
     return answer;
 }
 
@@ -78,14 +81,14 @@ string chooseWordFromList(const vector<string>& wordList, int index)
 string generateHiddenCharacters(string answerWord){
     // TODO: Based on answerWord's length, generate hidden characters in form of "---"
     string secretWord;
-
+    for(int i = 0; i < answerWord.size(); i++)secretWord += "-";
     return secretWord;
 }
 
 char getInputCharacter() {
     char ch;
     cin >> ch;
-    return tolower(ch); 
+    return tolower(ch);
 }
 
 /***
@@ -99,6 +102,14 @@ char getInputCharacter() {
 void updateSecretWord(string& secretWord, const char ch, const string& word)
 {
     // TODO: Update the secret word if the character ch is in the answer word.
+
+    for (int i = 0; i < word.size(); ++i) {
+
+        if (word[i] == ch) {
+            secretWord[i] = ch;
+        }
+    }
+
 }
 
 /***
@@ -110,6 +121,7 @@ void updateSecretWord(string& secretWord, const char ch, const string& word)
 ***/
 void updateEnteredChars(const char ch, string& chars){
     // TODO: append the character ch is in end of the text chars
+    chars += ch;
 }
 
 /***
@@ -120,6 +132,8 @@ void updateEnteredChars(const char ch, string& chars){
 ***/
 void updateIncorrectGuess(int& incorrectGuess){
     // TODO: increase the value of incorrectGuess by 1
+    incorrectGuess++;
+
 }
 
 /***
@@ -133,9 +147,9 @@ void updateIncorrectGuess(int& incorrectGuess){
     Returns:
         void
 ***/
-void processData(const char ch, const string& word, 
-                string& secretWord, 
-                string& correctChars, 
+void processData(const char ch, const string& word,
+                string& secretWord,
+                string& correctChars,
                 int& incorrectGuess, string& incorrectChars)
 {
     /*** TODO
@@ -146,5 +160,15 @@ void processData(const char ch, const string& word,
             update incorrectGuess: call updateIncorrectGuess() function
             update incorrectChars: call updateEnteredChars() function
     ***/
+    if(isCharInWord(const char ch,const string& word))
+    {
+        updateSecretWord(string& secretWord, const char ch, const string& word);
+        updateEnteredChars(const char ch, string& correctChars);
+    }
+    else{
+        updateIncorrectGuess(int& incorrectGuess);
+        updateEnteredChars(const char ch, string& incorrectChars);
+    }
+
 }
 
