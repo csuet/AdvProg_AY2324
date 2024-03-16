efcef1e5bd3ed67886fe86f82810ac088508a79a
huyen2k <huyennguyen08032005@gmail.com>
2024-03-16 14:53:52 +0700

    new

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -77,7 +77,7 @@ bool checkSuccess(string answer) {
 ***/
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
-    if(isContinued == 'Y') return 1;
+    if(isContinued == 'Y' || isContinued == 'y') return 1;
     return 0;
 }
 
