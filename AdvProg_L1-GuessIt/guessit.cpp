4a92edcb01d3a91d2d7c0179a3a6eddc1cb7fc1e
Dainn98 <tuananhk56a1@gmail.com>
2024-03-16 08:26:20 +0700

    pro 1

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -50,8 +50,8 @@ string getAnswer(int number, int randomNumber) {
               If number is equal randomNumber, the answer is "Congratulation! You win."
     ***/
     string answer;
-    if (number > randomNumber) answer = "Your number is lower.";
-    else if(number < randomNumber ) answer = "Your number is higher.";
+    if (number > randomNumber) answer = "Your number is higher.";
+    else if(number < randomNumber ) answer = "Your number is lower.";
     else answer = "Congratulation! You win.";
     return answer;
 }
