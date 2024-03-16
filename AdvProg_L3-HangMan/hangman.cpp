0eef0d0c05106da0314bac01894170a7527adc5e
HuyDang05 <quanghuy23052005@gmail.com>
2024-03-16 22:47:54 +0700

    final

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -99,7 +99,7 @@ string chooseWordFromList(const vector<string>& wordList, int index)
 string generateHiddenCharacters(string answerWord){
     // TODO: Based on answerWord's length, generate hidden characters in form of "---"
     string secretWord;
-    secretWord = string(answerWord.size(), '_');
+    secretWord = string(answerWord.size(), '-');
 
     return secretWord;
 
