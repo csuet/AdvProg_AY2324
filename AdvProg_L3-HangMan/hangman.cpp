7582bba816909a97a8d3e19328102c2154417914
huyen2k <huyennguyen08032005@gmail.com>
2024-03-16 15:21:40 +0700

    new

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -117,7 +117,10 @@ void updateSecretWord(string& secretWord, const char ch, const string& word)
 ***/
 void updateEnteredChars(const char ch, string& chars){
     // TODO: append the character ch is in end of the text chars
-    chars.push_back(ch);
+    string k = "";
+    k.push_back(ch);
+    k.push_back(' ');
+    chars = chars + k;
 }
 
 /***
