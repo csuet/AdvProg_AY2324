a7a895dd9a2a15dbeb8eafea3002dc6934bce5aa
ThePhapp <161786445+ThePhapp@users.noreply.github.com>
2024-03-18 22:02:31 +0700

    Update

--- AdvProg_L2-Calculus/calculus.cpp
+++ AdvProg_L2-Calculus/calculus.cpp
@@ -18,9 +18,22 @@ double mySqrt(double x);
     Returns:
         double: cosine of x
 ***/
-double myCos(double x) 
+double myCos(double x)
 {
-    return cos(x);
+    double old_value;
+    double value = 1;
+    int n = 1;
+    do{
+        old_value = value;
+        double mul = 1;
+        for (int i = 1; i < 2*n; i++){
+            mul*=i;
+        }
+        value += ( pow( -1, n)*pow( x, 2 * n))/mul;
+        n++;
+    }
+    while (value - old_value >= 0.1);
+    return value;
 }
 
 /***
@@ -31,7 +44,20 @@ double myCos(double x)
 ***/
 double mySin(double x)
 {
-    return sin(x);
+    double old_value;
+    double value = x;
+    int n = 1;
+    do{
+        old_value = value;
+        double mul = 1;
+        for (int i = 1; i < 2*n+1; i++){
+            mul*=i;
+        }
+        value += (pow(-1,n)*pow(x,2*n+1))/mul;
+        n++;
+    }
+    while (value - old_value >= 0.1);
+    return value;
 }
 
 
@@ -45,8 +71,7 @@ double mySqrt(double x) {
     if (x < 0) {
         cout << "Invalid argument" << endl;
         exit(1);
+    } else{
+        return sqrt(x);
     }
-
-    
-    return 0;
 }
