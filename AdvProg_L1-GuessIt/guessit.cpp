91366a3d4dc0a06bc5fdb17dc11e0d4cc9c40fc3
Hoang-Long <hoanglongjimmy2005@gmail.com>
2024-03-18 20:42:03 +0700

    guessit

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -53,7 +53,7 @@ string getAnswer(int number, int randomNumber) {
         answer = "Your number is higher.";
     else if( number < randomNumber )
         answer = "Your number is lower.";
-    else answer = "Congratulations! You win.";
+    else answer = "Congratulation! You win.";
     return answer;
 }
 
