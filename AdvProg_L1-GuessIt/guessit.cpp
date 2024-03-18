5759966dd92617deec4c0ee1ca6ad1e3f2522daa
Hoang-Long <hoanglongjimmy2005@gmail.com>
2024-03-18 22:20:44 +0700

    guessit

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -80,7 +80,7 @@ bool checkSuccess(string answer) {
 ***/
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
-    if( isContinued == 'Y' )
+    if( isContinued == 'Y' || isContinued == 'y')
         return true;        
     else return false;
 }
