1a27c9dd895170c1ff18d6ad97f931b87c247545
Hoang-Long <hoanglongjimmy2005@gmail.com>
2024-03-19 21:17:57 +0700

    hangman

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -131,7 +131,7 @@ void updateSecretWord(string& secretWord, const char ch, const string& word)
 ***/
 void updateEnteredChars(const char ch, string& chars){
     // TODO: append the character ch to the end of the text chars
-    chars.push_back(ch);
+    chars = chars + ch + " '.";
 }
 
 /***
