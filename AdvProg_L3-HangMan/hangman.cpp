8ade464ebf6fe1ac46e62afb2dbf72b3cd17275e
Hoang-Long <hoanglongjimmy2005@gmail.com>
2024-03-19 21:05:14 +0700

    hangman

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -90,7 +90,7 @@ string generateHiddenCharacters(string answerWord){
     // TODO: Based on answerWord's length, generate hidden characters in form of "---"
     string secretWord;
     for(int i = 0; i < answerWord.size(); i++)
-        secretWord += "_";
+        secretWord += "-";
     return secretWord;
 }
 
