632807774b4d1bbedf1077a5d5bed985b899fa58
phung1811 <161928550+phihungcn1uet@users.noreply.github.com>
2024-03-19 19:49:30 +0700

    Update hello.cpp

--- AdvProg_L0-Hello/hello.cpp
+++ AdvProg_L0-Hello/hello.cpp
@@ -1,37 +1,7 @@
 #include <iostream>
-#include <vector>
+#include "hello.h"
 
-using namespace std;
-
-void generateWords(int N, int K, string word, vector<bool>& usedChars) {
-    if (K == 0) {
-        cout << word << endl;
-        return;
-    }
-
-    for (int i = 0; i < N; i++) {
-        if (!usedChars[i]) {
-            usedChars[i] = true;
-            generateWords(N, K - 1, word + char('a' + i), usedChars);
-            usedChars[i] = false;
-        }
-    }
-}
-
-int main() {
-    int N, K;
-    cin >> N;
-    cin >> K;
-
-    if (N < K || N > 26 || K <= 0) {
-        cout << "-1" << endl;
-        return 0;
-    }
-
-    vector<bool> usedChars(N, false);
-    string word = "";
-
-    generateWords(N, K, word, usedChars);
-
-    return 0;
+std::string printGameOver(){
+	// TODO: Return the required string
+	return "Game Over!!!";
 }
