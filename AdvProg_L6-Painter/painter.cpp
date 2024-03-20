a5d5179237895ba167ad10b4362b1f82e49cd4fe
23020157tranthiphuongthao <161732192+23020157tranthiphuongthao@users.noreply.github.com>
2024-03-20 20:35:57 +0700

    Update painter.cpp

--- AdvProg_L6-Painter/painter.cpp
+++ AdvProg_L6-Painter/painter.cpp
@@ -51,7 +51,7 @@ void Painter::jumpBackward(int numPixel)
 void Painter::turnLeft(double degree)
 {
     // TODO: rotate left the painter
-     this->angle= angle+degree - floor(angle/360)*360;
+     setAngle(this->angle+degree);
 
 
 }
