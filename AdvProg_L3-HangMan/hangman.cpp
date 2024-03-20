696f0d9e4e5fa75a1a7c2e8856de9f6bae51a74b
ThePhapp <161786445+ThePhapp@users.noreply.github.com>
2024-03-20 22:07:50 +0700

    Update

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -92,7 +92,7 @@ string chooseWordFromList(const vector<string>& wordList, int index)
 string generateHiddenCharacters(string answerWord){
     // TODO: Based on answerWord's length, generate hidden characters in form of "---"
     string secretWord;
-    for(int i =0; i<secretWord.size(); i++)
+    for(int i =0; i<answerWord.size(); i++)
         {
             secretWord += '-';
         }
