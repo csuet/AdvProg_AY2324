f589d53af7cf509ec9d23e67e5131645701e5a62
Pham Hung <hungphamvp09@gmail.com>
2024-03-20 20:52:36 +0700

    hang man

--- AdvProg_L0-Hello/main.cpp
+++ AdvProg_L0-Hello/main.cpp
@@ -1,6 +1,26 @@
-#include <iostream>
-#include "hello.h"
+#include<bits/stdc++.h>
+using namespace std;
+int f(int a[],int n)
+{
+    if(n=0) return a[n];
+    return a[n]+a[n-1];
+}
+int main()
+{   
+    freopen("test.inp","r",stdin);
+    freopen("test.out","w",stdout);
+    int n;cin>>n;
+    int a[n];
+    int b[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    sort(a,a+n);
+    for(int i=0;i<n;i++)
+    {
+        b[i]=f(a,i);
+        cout<<b[i]<<" ";
+    }
 
-int main(){
-    std::cout << printGameOver() << std::endl ;
 }
