0bbeb924863054e8dfeeb4349a38592bfd1fa09f
tunglamp08072005 <tunglamp227@gmail.com>
2024-03-21 16:53:33 +0700

    lec3(07)

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -118,7 +118,10 @@ void updateSecretWord(string& secretWord, const char ch, const string& word)
 ***/
 void updateEnteredChars(const char ch, string& chars){
     // TODO: append the character ch is in end of the text chars
-    chars = chars + " ";
+    if(chars.find(ch) == string::npos)
+    {
+        chars += ch;
+    }
 }
 
 /***
