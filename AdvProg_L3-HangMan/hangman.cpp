af68220f61f57098083d8d1191456ae44fb200d7
DaoNangDiu <daonangdiu28102005@gmail.com>
2024-03-27 01:54:35 +0700

    g

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -72,7 +72,7 @@ string chooseWordFromList(const vector<string>& wordList, int index)
     string answer;
     answer = wordList[index];
     for ( int i=0 ; i < answer.length() ; i++ )
-    tolower(answer[i]);
+    answer[i]=tolower(answer[i]);
     return answer;
 }
 
