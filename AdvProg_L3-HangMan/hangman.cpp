a78537267e3a5cbe8a55bbe8b47c34165de754f4
nkhanhhh <iamkhanhpham11@gmail.com>
2024-03-26 23:35:03 +0700

    he

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -7,144 +7,73 @@ using std::ifstream;
 using std::domain_error;
 using std::cin;
 
-/***
-    Args:
-        min (int): left margin of a range
-        max (int): right margin of a range
-    Returns:
-        number (int) : random number in range [min; max]
-***/
 int generateRandomNumber(const int min, const int max)
 {
     // TODO: Return a random integer number between min and max
     return 1;
 }
 
-vector<string> readWordListFromFile(const string& filePath)
+std::vector<std::string> readWordListFromFile(const std::string& filePath)
 {
-    vector<string> wordList;
-    string word;
-    ifstream wordFile (filePath);
+    std::vector<std::string> wordList;
+    std::string word;
+    std::ifstream wordFile(filePath);
     if (!wordFile.is_open()) {
-        throw domain_error("Unable to open file");
+        throw std::domain_error("Unable to open file");
     }
 
-    //while ( getline (wordFile, word) ){  // Thong thuong doc tung line. 
-                                           // Chuong trinh nay cung chay.
-    while (wordFile >> word) {  // Nhung voi chuong trinh nay, doc tung word cung duoc
-                                // Tuc ca 2 cach doc deu chay.
+    while (wordFile >> word) {
         wordList.push_back(word);
-        //cout << word << '\n';
     }
     wordFile.close();
 
     return wordList;
 }
 
-/***
-    Args:
-        ch (char): A character
-        word (string): a word
-    Returns:
-        result (bool) : the character ch is in the word or not.
-***/
-bool isCharInWord(const char ch, const string& word)
+bool isCharInWord(const char ch, const std::string& word)
 {
     // TODO: return true if ch is in word else return false
     return true;
 }
 
-/***
-    Args:
-        wordList (vector<string>): A list of words
-        index (int): an integer number
-    Returns:
-        answer (string) : the lowercase word is in the position index of wordList
-***/
-string chooseWordFromList(const vector<string>& wordList, int index) 
+std::string chooseWordFromList(const std::vector<std::string>& wordList, int index)
 {
     // TODO: Return a lowercase word in the index position of the vector wordList.
-    string answer;
+    std::string answer;
 
     return answer;
 }
 
-/***
-    Args:
-        answerWord (string): a word that player needs to guess
-    Returns:
-        secretWord (string): answerWord in hidden form (form of ---)
-***/
-string generateHiddenCharacters(string answerWord){
-    // TODO: Based on answerWord's length, generate hidden characters in form of "---"
-    string secretWord;
-
+std::string generateHiddenCharacters(const std::string& answerWord)
+{
+    // TODO: Generate a hidden version of answerWord (e.g., "apple" -> "-----")
+    std::string secretWord;
+    for (char ch : answerWord) {
+        if (std::isalpha(ch)) {
+            secretWord += '-';
+        }
+        else {
+            secretWord += ch;
+        }
+    }
     return secretWord;
 }
 
-char getInputCharacter() {
-    char ch;
-    cin >> ch;
-    return tolower(ch); 
-}
-
-/***
-    Args:
-        secretWord (string): secret word in hidden form
-        ch (char): a charater
-        word (string): the answer word
-    Returns:
-        void
-***/
-void updateSecretWord(string& secretWord, const char ch, const string& word)
+int main()
 {
-    // TODO: Update the secret word if the character ch is in the answer word.
-}
+    // Example usage
+    std::string filePath = "wordlist.txt";
+    std::vector<std::string> wordList = readWordListFromFile(filePath);
 
-/***
-    Args:
-        ch (char): a character
-        chars (string): an array of characters
-    Returns:
-        void
-***/
-void updateEnteredChars(const char ch, string& chars){
-    // TODO: append the character ch is in end of the text chars
-}
+    // Choose a random word from the list
+    int randomIndex = generateRandomNumber(0, wordList.size() - 1);
+    std::string chosenWord = chooseWordFromList(wordList, randomIndex);
 
-/***
-    Args:
-        incorrectGuess (int): a number that store the number of player's wrong guess
-    Returns:
-        void
-***/
-void updateIncorrectGuess(int& incorrectGuess){
-    // TODO: increase the value of incorrectGuess by 1
-}
+    // Generate the hidden version of the chosen word
+    std::string hiddenWord = generateHiddenCharacters(chosenWord);
 
-/***
-    Args:
-        ch (char): a character that player enter to console
-        word (string): answer word that play needs to guess
-        secretWord (string): answer word in hidden form
-        correctChars (string): a string that stores correct inputs of player
-        incorrectGuess (int): a number that stores the number of player's wrong guess
-        incorrectChars (string): a string that stores incorrect inputs of player
-    Returns:
-        void
-***/
-void processData(const char ch, const string& word, 
-                string& secretWord, 
-                string& correctChars, 
-                int& incorrectGuess, string& incorrectChars)
-{
-    /*** TODO
-        If ch in word:
-            update secretWord: call updateSecretWord() function
-            update correctChars: call updateEnteredChars() function
-        else:
-            update incorrectGuess: call updateIncorrectGuess() function
-            update incorrectChars: call updateEnteredChars() function
-    ***/
-}
+    std::cout << "Chosen word: " << chosenWord << std::endl;
+    std::cout << "Hidden word: " << hiddenWord << std::endl;
 
+    return 0;
+}
