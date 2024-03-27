643483bbb9b269a0d4241a0cba921575730d3323
hungdung29 <dung292005@gmail.com>
2024-03-27 17:21:42 +0700

    update hangman

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -73,7 +73,7 @@ string chooseWordFromList(const vector<string>& wordList, int index)
     string answer;
     answer = wordList[index];
     for (int i = 0; i < answer.size(); i++) {
-        tolower(answer[i]);
+        answer[i]=tolower(answer[i]);
     }
     return answer;
 }
