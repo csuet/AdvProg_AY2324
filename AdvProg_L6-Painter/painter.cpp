7ef493fdeb31090894fb8fac016e5fac8b7b3655
Huy1902 <lymstar19@gmail.com>
2024-03-27 08:13:59 +0700

    painter

--- AdvProg_L6-Painter/painter.cpp
+++ AdvProg_L6-Painter/painter.cpp
@@ -9,6 +9,7 @@
 void Painter::setColor(SDL_Color color) 
 { 
     // TODO: set the color value for the Painter and set Render Draw Color
+    SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b);
 }
 
 
@@ -21,6 +22,7 @@ void Painter::setColor(SDL_Color color)
 void Painter::jumpForward(int numPixel)
 {
     // TODO: jump the painter forward
+    moveForward(numPixel);
 }
 
 
@@ -33,6 +35,7 @@ void Painter::jumpForward(int numPixel)
 void Painter::jumpBackward(int numPixel)
 {
     // TODO: jump the painter backward
+    moveBackward(numPixel);
 }
 
 
@@ -45,6 +48,7 @@ void Painter::jumpBackward(int numPixel)
 void Painter::turnLeft(double degree)
 {
     // TODO: rotate left the painter   
+    angle += degree;
 }
 
 
@@ -57,6 +61,7 @@ void Painter::turnLeft(double degree)
 void Painter::turnRight(double degree)
 {
     // TODO: rotate right the painter   
+    angle -= degree;
 }
 
 /***  
@@ -67,7 +72,11 @@ void Painter::turnRight(double degree)
 ***/
 void Painter::randomColor()
 {
-    // TODO: set random color    
+    // TODO: set random color  
+    Uint8 r = rand() % 256;
+    Uint8 g = rand() % 256;
+    Uint8 b = rand() % 256;
+    color = { r, g, b };
 }
 
 
