6eff2d0c409c732e0bdbef49e0b80ba54a2bd9b8
Huy1902 <lymstar19@gmail.com>
2024-03-27 10:11:30 +0700

    painter

--- AdvProg_L6-Painter/painter.cpp
+++ AdvProg_L6-Painter/painter.cpp
@@ -22,7 +22,7 @@ void Painter::setColor(SDL_Color color)
 void Painter::jumpForward(int numPixel)
 {
     // TODO: jump the painter forward
-    double radian = angle * 3.141592654 / 180.0;
+    double radian = angle * 3.141592653589793 / 180.0;
     x += numPixel * (int)cos(radian);
     y += numPixel * -(int)sin(radian);
 }
