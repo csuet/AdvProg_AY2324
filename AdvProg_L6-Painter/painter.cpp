ed6ec48e277619e2fb73a5b59d49bc0bf816988a
dangdd2208 <dinhdang2208@gmail.com>
2024-03-29 16:57:37 +0700

    hi

--- AdvProg_L6-Painter/painter.cpp
+++ AdvProg_L6-Painter/painter.cpp
@@ -54,7 +54,7 @@ void Painter::jumpBackward(int numPixel)
 void Painter::turnLeft(double degree)
 {
     // TODO: rotate left the painter  
-    degree += this->angle ;
+  degree += this->angle ;
   this->angle = degree - floor(degree/360)*360;
 
  
@@ -70,8 +70,7 @@ void Painter::turnLeft(double degree)
 void Painter::turnRight(double degree)
 {
     // TODO: rotate right the painter 
-     degree -= this->angle ;
- 	 this->angle = -degree + floor(degree/360)*360;
+    turnLeft(-degree);
 }
 
 /***  
