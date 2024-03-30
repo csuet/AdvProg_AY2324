dd46102f02aa106df87d9a06fece4f96b2ef41d3
trinhxuanhoa <bongnaiton30@gmail.com>
2024-03-30 09:08:39 +0700

    hm3

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -55,10 +55,10 @@ bool isCharInWord(const char ch, const string& word)
     // TODO: return true if ch is in word else return false
     for (int i = 0; i < word.length(); i++) {
         if (ch!=word[i])
-return false;
+return true;
     }
 
-    return true;
+    return false;
 }
 
 /***
