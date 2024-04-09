1991be9f75b2afaedb9ebc93fe09b0c907e1c214
top1server <mightdung7105@gmail.com>
2024-04-09 22:03:58 +0700

    T

--- AdvProg_L4-SimpleAI/simpleai.cpp
+++ AdvProg_L4-SimpleAI/simpleai.cpp
@@ -174,7 +174,7 @@ bool isWholeWord(const string& mask)
      }
      for (int i = 0; i < (int)mask.size(); i++)
      {
-         if (mask[i] <= 'a' || mask[i] >= 'z')
+         if (mask[i] <= 'a' || mask[i] >= 'z' || mask[i] == '_')
          {
              answer = false;
          }
