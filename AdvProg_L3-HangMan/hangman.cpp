1286ea8cf0cad18c89123aed43db99caa2f5baef
Mac <mac@Macs-MacBook-Pro.local>
2024-04-11 08:51:15 +0700

    update

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -87,7 +87,8 @@ string chooseWordFromList(const vector<string>& wordList, int index)
 string generateHiddenCharacters(string answerWord){
     // TODO: Based on answerWord's length, generate hidden characters in form of "---"
     string secretWord;
-    for(int i=0; i<secretWord.size(); i++)
+    secretWord = answerWord;
+    for(int i=0; i<answerWord.size(); i++)
     {
         secretWord[i]='-';
     }
