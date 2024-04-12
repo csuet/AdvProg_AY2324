ec81d7f3b664fd81cb83d9fc6a21e53e8b50cc62
Miaan <nghiabn2005@gmail.com>
2024-04-12 07:31:43 +0700

    Finish L2

--- AdvProg_L2-Calculus/calculus.cpp
+++ AdvProg_L2-Calculus/calculus.cpp
@@ -18,9 +18,20 @@ double mySqrt(double x);
     Returns:
         double: cosine of x
 ***/
-double myCos(double x) 
+double myCos(double x)
 {
-    return 0.0;
+//    double pi = 3.14159265358979323846;
+//    x = x * pi / 180;
+    double res = 1;
+    double add = 1;
+    for(int i = 1;; i++)
+    {
+        add *= -1;
+        add *= (x * x) / (2 * i * (2 * i - 1));
+        if(std::abs(add) < 0.1e-200) break;
+        res += add;
+    }
+    return res;
 }
 
 /***
@@ -31,7 +42,11 @@ double myCos(double x)
 ***/
 double mySin(double x)
 {
-    return 0.0;
+    double y = myCos(x);
+    double ans = 1;
+    ans -= y * y;
+    ans = mySqrt(ans);
+    return ans;
 }
 
 
@@ -47,6 +62,16 @@ double mySqrt(double x) {
         exit(1);
     }
 
-    
-    return 0;
+    if(x == 0) return 0;
+
+    double ans = 0;
+    double guess = x;
+    while(1)
+    {
+        ans = 0.5 * (guess + (x / guess));
+        if(std::abs(ans - guess) < 1e-10) break;
+        guess = ans;
+    }
+
+    return ans;
 }
