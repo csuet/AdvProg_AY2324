eb0470bd4c4f1827e6fc17fb373f0bf682969872
bthdz <hieubui442005@gmail.com>
2024-04-12 22:37:07 +0700

    simpleAI

--- AdvProg_L4-SimpleAI/simpleai.h
+++ AdvProg_L4-SimpleAI/simpleai.h
@@ -12,11 +12,11 @@ using namespace std;
 
 int readMaxGuess();
 int readWordLen();
-static vector<string> filterWordsByLen(int wordLen, const vector<string>& vocabulary);
-static char findBestChar(const vector<string>& candidateWords, const set<char>& selectedChars);
-static string getWordMask(char nextChar);
-static bool isCorrectChar(char ch, const string& mask);
-static bool isWholeWord(const string& mask);
-static vector<string> filterWordsByMask(const vector<string>& words, const string& mask, char ch);
+vector<string> filterWordsByLen(int wordLen, const vector<string>& vocabulary);
+char findBestChar(const vector<string>& candidateWords, const set<char>& selectedChars);
+string getWordMask(char nextChar);
+bool isCorrectChar(char ch, const string& mask);
+bool isWholeWord(const string& mask);
+vector<string> filterWordsByMask(const vector<string>& words, const string& mask, char ch);
 
 const char MASK_CHAR = '-';
