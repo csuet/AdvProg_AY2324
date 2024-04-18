ad812e485bbdc91ee2da03f996c7bc0a5abce07e
Nguyen Viet Lam <nguyenlam25022005@gmail.com>
2024-04-19 01:41:37 +0700

    10th commit

--- AdvProg_L4-SimpleAI/simpleai.cpp
+++ AdvProg_L4-SimpleAI/simpleai.cpp
@@ -108,8 +108,7 @@ char findBestChar(const vector<string>& candidateWords, const set<char>& selecte
 {
     char answer = 0;
     //Write your code here
-    map<char,int> occurrences = countOccurrences(candidateWords);
-  //  answer = findMostFrequentChar(occurrences,selectedChars);
+    answer = findMostFrequentChar(countOccurrences(candidateWords),selectedChars);
     return answer;
 }
 
