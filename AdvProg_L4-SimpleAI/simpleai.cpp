b23176767c93a1a42195ae5302ee9eabd2a8e7f9
Nguyen Viet Lam <nguyenlam25022005@gmail.com>
2024-04-19 11:15:41 +0700

    17th commit

--- AdvProg_L4-SimpleAI/simpleai.cpp
+++ AdvProg_L4-SimpleAI/simpleai.cpp
@@ -87,10 +87,10 @@ char findMostFrequentChar(const map<char, int>& occurrences, const set<char>& se
     int mx = 0;
     for(char c = 'a';c <= 'z';++c){
         if (selectedChars.find(c) == selectedChars.end()){
-    //         if (occurrences.at(c) > mx){
+            if (occurrences.at(c) > mx){
     //             answer = c;
     //             mx = occurrences.at(c);
-    //         }
+            }
         }
     }
     return answer;
