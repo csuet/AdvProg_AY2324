bad6c6c9108f22f7943094b9570d9db396607c3c
Dang Dinh Khang <lhang18022005@gmail.com>
2024-04-23 15:18:57 +0700

    finish snake

--- AdvProg_L7-Snake/Game.cpp
+++ AdvProg_L7-Snake/Game.cpp
@@ -51,12 +51,19 @@ Game::~Game()
 ***/
 
 void Game::snakeMoveTo(Position pos) {
-	//  START CODE HERE
-	//
-	//
-	//
-	//
-	// END CODE HERE
+	
+	switch (getCellType(pos))
+	{
+		case CELL_SNAKE:
+		case CELL_CHERRY:
+			score++;
+			snake.eatCherry();
+			addCherry();
+			break;
+		default:
+			snake.slideTo(pos);
+			break;
+	}
 }
 
 
@@ -73,11 +80,9 @@ void Game::snakeMoveTo(Position pos) {
 void Game::snakeLeave(Position position)
 {
 	// Suggestion: use setCellType() method in Game class
-	// START CODE HERE
-	//  
-	//
-	//
-	// END CODE HERE
+	
+	setCellType(position, CELL_EMPTY);
+
 }
 
 
@@ -103,9 +108,11 @@ void Game::processUserInput(Direction direction)
  * 
  ***/
 bool Game::canChange(Direction current, Direction next) const {
-	if (current == UP || current == DOWN) 
-		return 0; // YOUR CODE HERE
-	return 0;// YOUR CODE HERE
+	if ((current == UP || current == DOWN) && (next == UP || next == DOWN) || (current == LEFT || current == RIGHT) && (next == LEFT || next == RIGHT)) 
+	{
+		return false;
+	}
+	return true;
 }
 
 
@@ -128,14 +135,17 @@ void Game::nextStep()
 {
 	while (!inputQueue.empty()) {
 		// get the input direction from input queue
+		
         Direction next ; // YOUR CODE HERE
 
 		// remove the front of input queue
         // YOUR CODE HERE
-
+		next = inputQueue.front();
+		inputQueue.pop();
 		// check if snake can move to the next direction, set current direction as next
         if (canChange(currentDirection, next)) {
         	// YOUR CODE HERE
+			currentDirection = next;
         	break;
 		}
     }
@@ -163,15 +173,11 @@ void Game::addCherry()
 		// Suggestion: use rand() function
 
         Position randomPos; // YOUR CODE HERE
-		
+		randomPos = Position(rand()%width, rand()%height);
 		// check if the randomPos is EMPTY 
-        if (getCellType(randomPos) == CELL_EMPTY) {
-
-        	// assign the cherry position as randomPos, and set randomPos type as CELL_CHERRY
-
-			// YOUR CODE HERE
-			// YOUR CODE HERE
-
+        if (getCellType(randomPos) == CELL_EMPTY) {  //neu cell rong;
+			cherryPosition = randomPos;
+			setCellType(randomPos, CELL_CHERRY);
        		break;
         }
     } while (true);
@@ -196,9 +202,10 @@ void Game::setCellType(Position pos, CellType cellType)
 	// Otherwise, do nothing
 	// Suggestion: use pos.isInsideBox(...) in Position class
 	//
-	// START CODE HERE
-	//  
-	// END CODE HERE
+	if (pos.isInsideBox(0,0, width, height))
+	{
+		squares[pos.x][pos.y] = cellType;
+	}
 }
 
 
--- AdvProg_L7-Snake/Snake.cpp
+++ AdvProg_L7-Snake/Snake.cpp
@@ -25,6 +25,13 @@ Snake::~Snake()
             p = nextNode;
     }
     */
+   SnakeNode* p = tail;
+   while (p != nullptr)
+   {
+        SnakeNode* nextNode = p->next;
+        delete p;
+        p = nextNode;
+   }
 }
 
 // DO NOT CHANGE METHOD
@@ -52,8 +59,8 @@ vector<Position> Snake::getPositions() const
 void Snake::growAtFront(Position newPosition)
 {
     // head of snake grow at new position
-	
-    /* YOUR CODE HERE */
+	head->next = new SnakeNode(newPosition);   //cho head tro toi next
+    head = head->next;
 }
 
 
@@ -83,15 +90,14 @@ void Snake::slideTo(Position newPosition)
 {
 	if (tail->next == nullptr) { 
         // position is assigned by new position.
-		/* YOUR CODE HERE */
+		tail->position = newPosition;
 	}
 	else {
 		SnakeNode *oldTailNode = tail;
 		//cut the old tail off the snake
-        /* YOUR CODE HERE */
-		
-		// move it to the head of the snake
-        /* YOUR CODE HERE */
+        tail = tail->next;
+        oldTailNode->position = newPosition;
+        head->next = oldTailNode;
 		head = oldTailNode;
 	}
 }
@@ -110,7 +116,7 @@ void Snake::slideTo(Position newPosition)
 ***/
 void Snake::eatCherry()
 {
-	/* YOUR CODE HERE */
+	cherry++;
 }
 
 /*** 
@@ -144,16 +150,17 @@ void Snake::move(Direction direction)
     Position newPosition = head->position.move(direction);
 
     /* YOUR CODE HERE */
-    
+    game.snakeMoveTo(newPosition);
     // If gameOver, return ; 
     /* YOUR CODE HERE */
-
+    if (game.isGameOver()) return;
     // If cherry > 0, cherry descrease one and growAtFront() with newPosition
     if (cherry > 0) {
-        /* YOUR CODE HERE */
+        cherry--;
+        growAtFront(newPosition);
     } else {
     	game.snakeLeave(tail->position);
-        /* YOUR CODE HERE */        
+        slideTo(newPosition);   
     }
 }
 
