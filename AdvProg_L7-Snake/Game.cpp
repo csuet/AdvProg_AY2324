ab6c6507973d38e3f2a47b5b1b97ddbf506cfe90
bthdz <hieubui442005@gmail.com>
2024-04-27 16:41:59 +0700

    updated

--- AdvProg_L7-Snake/Game.cpp
+++ AdvProg_L7-Snake/Game.cpp
@@ -49,7 +49,9 @@ Game::~Game()
  * 		// none
  *
 ***/
-
+GameStatus Game::getGameStatus(){
+	return status;
+}
 void Game::snakeMoveTo(Position pos) {
 	//  START CODE HERE
 	//
@@ -234,9 +236,7 @@ vector<Position> Game::getSnakePositions() const
 }
 
 // DO NOT change this method
-GameStatus Game::getGameStatus(){
-	return status;
-}
+
 
 // DO NOT change this method
 int Game::getWidth(){
