255593513b289633950c00429299e2a7349fdd76
HoangPhu <phuhv102@gmail.com>
2024-05-04 00:51:45 +0700

    L6 v3

--- AdvProg_L6-Painter/painter.cpp
+++ AdvProg_L6-Painter/painter.cpp
@@ -9,7 +9,7 @@
 void Painter::setColor(SDL_Color color) 
 { 
     // TODO: set the color value for the Painter and set Render Draw Color
-    SDL_SetRenderDrawColor(renderer, color->r, color->g, color->b, 0xFF);
+    SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, 0xFF);
 }
 
 
