9e29efefa31e47723806b2b7bd3a1177dfdcc6a7
trinhxuanhoa <bongnaiton30@gmail.com>
2024-05-04 10:40:59 +0700

    s5

--- AdvProg_L7-Snake/Snake.cpp
+++ AdvProg_L7-Snake/Snake.cpp
@@ -4,22 +4,23 @@
 
 void Snake::growAtFront(Position newPosition)
 {
-    SnakeNode *newHead = new SnakeNode(newPosition, nullptr);
-    head->next = newHead;
+    // head of snake grow at new position
+    SnakeNode *newHead = new SnakeNode(newPosition, head);
     head = newHead;
 }
 
 void Snake::slideTo(Position newPosition)
 {
-    if (tail->next == nullptr)
-    {
+    if (tail->next == nullptr) {
+        // snake has only one node
+        // tail->position is assigned by new position.
         tail->position = newPosition;
-    }
-    else
-    {
+    } else {
         SnakeNode *oldTailNode = tail;
+        // cut the old tail off the snake
         tail = tail->next;
         oldTailNode->next = nullptr;
+        // move it to the head of the snake
         oldTailNode->position = newPosition;
         head->next = oldTailNode;
         head = oldTailNode;
@@ -34,19 +35,32 @@ void Snake::eatCherry()
 void Snake::move(Direction direction)
 {
     Position newPosition = head->position.move(direction);
+
     game.snakeMoveTo(newPosition);
 
     if (game.isGameOver())
         return;
 
-    if (cherry > 0)
-    {
+    if (cherry > 0) {
         cherry--;
         growAtFront(newPosition);
-    }
-    else
-    {
+    } else {
         game.snakeLeave(tail->position);
         slideTo(newPosition);
     }
+}
+
+// DO NOT CHANGE METHOD
+int Snake::getNumCherry(){
+    return cherry;
+}
+
+// DO NOT CHANGE METHOD
+SnakeNode* Snake::getHead(){
+    return head;
+}
+
+// DO NOT CHANGE METHOD
+SnakeNode* Snake::getTail(){
+    return tail;
 }
