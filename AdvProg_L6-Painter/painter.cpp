e3f67da6d69e5b1efebc4e266cc046984b002777
Nguyen Bao Long <longlam0401@gmail.com>
2024-05-07 21:17:12 +0700

    haha

--- AdvProg_L6-Painter/painter.cpp
+++ AdvProg_L6-Painter/painter.cpp
@@ -52,7 +52,7 @@ void Painter::jumpBackward(int numPixel)
 void Painter::turnLeft(double degree)
 {
     // TODO: rotate left the painter   
-    angle += degree;
+    Painter.setAngle(angle + degree);
 }
 
 
@@ -65,7 +65,7 @@ void Painter::turnLeft(double degree)
 void Painter::turnRight(double degree)
 {
     // TODO: rotate right the painter   
-    angle -= degree;
+    Painter.setAngle(angle - degree);
 }
 
 /***  
