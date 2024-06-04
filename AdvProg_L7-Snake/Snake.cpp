59efc8c63819ef7944b5a1e933b6caf849738312
dang0801205 <letridangg2005@gmail.com>
2024-06-04 18:53:59 +0800

    df

--- AdvProg_L7-Snake/Snake.cpp
+++ AdvProg_L7-Snake/Snake.cpp
@@ -1,252 +1,201 @@
-#include <vector>
-#include <cassert>
-#include <cstdlib>
-#include <iostream>
+#include "Snake.h"
 #include "Game.h"
-
-using namespace std;
+#include <iostream>
 
 // set some attributes as default value
 // DO NOT CHANGE THIS CONSTRUCTOR
-Game::Game(int _width, int _height)
-	: width(_width), height(_height),						  // play screen
-	  squares(_height, vector<CellType>(_width, CELL_EMPTY)), // cell coordinates
-	  snake(*this, Position(_width / 2, _height / 2)),		  // init snake positin in middle of play screen
-	  currentDirection(Direction::RIGHT),
-	  status(GAME_RUNNING),
-	  score(0)
+Snake::Snake(Game &_game, Position start)
+    : head(new SnakeNode(start)), tail(head), game(_game), cherry(0)
 {
-	// add new cheery in game initiation
-	addCherry();
+    // Using Game.snakeMoveTo method to move Snake to start position;
+    game.snakeMoveTo(start);
 }
 
-Game::~Game()
+// set some attributes as default value
+// PLEASE UPDATE THIS METHOD
+
+Snake::~Snake()
 {
-	// dtor
+    /*
+        Loop: SnakeNode p = tail; p != nullptr;
+
+        Do:
+            SnakeNode* nextNode = p->next;
+            // delete p;
+            p = nextNode;
+    }
+    */
+    for (SnakeNode *p = tail; p != nullptr;)
+    {
+        SnakeNode *nextNode = p->next;
+        delete p;
+        p = nextNode;
+    }
+}
+
+// DO NOT CHANGE METHOD
+vector<Position> Snake::getPositions() const
+{
+    vector<Position> res;
+    for (SnakeNode *p = tail; p != nullptr; p = p->next)
+        res.push_back(p->position);
+    return res;
 }
 
 /***
  * PLEASE UPDATE THIS METHOD
  *
  * When snake moves to a position,
- * if position belongs to BOARD or SNAKE body, status is GAME_OVER
- * if position is having CHERRY :
- * 			- score schoule be increased
- * 			- snake should eat cherry
- * 			- a new cherry should be randomly added
- * otherwise, this position should be assigned as cell of snake
- *
+ *  head->next = ...
+ *  head = head->next;
  * Args:
- * 		pos (Position): position where the snake will move
+ * 		pos (newPosition): head of snake grow at new position
  *
  * Returns:
  * 		// none
  *
  ***/
-
-void Game::snakeMoveTo(Position pos)
+void Snake::growAtFront(Position newPosition)
 {
-	//  START CODE HERE
-	if (!pos.isInsideBox(0, 0, width, height) || getCellType(pos) == CELL_OFF_BOARD || getCellType(pos) == CELL_SNAKE)
-	{
-		status = GAME_OVER;
-	}
-	else if (getCellType(pos) == CELL_CHERRY)
-	{
-		++score;
-		snake.eatCherry();
-		addCherry();
-	}
-	else
-	{
-		snake.growAtFront(pos);
-	}
-	// END CODE HERE
+    // head of snake grow at new position
+
+    /* YOUR CODE HERE */
+    SnakeNode *newHead = new SnakeNode(newPosition, nullptr);
+    head->next = newHead;
+    head = newHead;
 }
 
 /***
  * PLEASE UPDATE THIS METHOD
  *
- * When all snake body leave a cell, set it as CELL_EMPTY
+ * When snake slide to a newPosition,
+ *  if tail->next == nullptr: // snake has only one node
+ *      // tail->position is assigned by new position.
+ *  else:
+ *      SnakeNode *oldTailNode = tail;
+ *      //cut the old tail off the snake
+ *      tail = tail->next;
+ *		oldTailNode->next = nullptr;
+ *      // move it to the head of the snake
+ *      oldTailNode position = newPosition;
+ *  	head->next = oldTailNode;
  * Args:
- * 		position (Position): position where snake will leave
- * Returns:
- * 		// none
+ * 		pos (newPosition): head of snake grow at new position
  *
- ***/
-void Game::snakeLeave(Position position)
-{
-	// Suggestion: use setCellType() method in Game class
-	// START CODE HERE
-	setCellType(position, CELL_EMPTY);
-	// END CODE HERE
-}
-
-// DO NOT change this method
-void Game::processUserInput(Direction direction)
-{
-	inputQueue.push(direction);
-}
-
-/***
- * PLEASE REPLACE LINES MARKED WITH '// YOUR CODE HERE'
- *
- * check whether the snake can move to the intended direction with the currect direction.
- * If current direction is UP or DOWN, the next direction should not be UP or DOWN
- * if current diection is LEFT or RIGHT, the next direction should not be LEFT or RIGHT
- *
- * Args:
- * 		current (Direction): current direction of the snake
- * 		next (Direction): the intened direction that snake will move
- * Returns:
- * 		bool: whether the snake can ben changed the direction
- *
- ***/
-bool Game::canChange(Direction current, Direction next) const
-{
-	if ((current == UP || current == DOWN) && (next == UP || next == DOWN)) // YOUR CODE HERE
-	{
-		return false;
-	}
-	if ((current == LEFT || current == RIGHT) && (next == LEFT || next == RIGHT))
-	{
-		return false;
-	}
-	return true; // YOUR CODE HERE
-}
-
-/***
- * PLEASE REPLACE LINES MARKED WITH '// YOUR CODE HERE'
- *
- * Iterover the input queue from keyboard.
- * For each input direction, check whether snake can move or not.
- * If the snake can move,  set currentDiection as this input direction.
- * Otherwise, go to the next input direction from input queue
- *
- * Args:
- * 		// none
  * Returns:
  * 		// none
  *
  ***/
 
-void Game::nextStep()
+void Snake::slideTo(Position newPosition)
 {
-	while (!inputQueue.empty())
-	{
-		// get the input direction from input queue
-		// YOUR CODE HERE
-		Direction next = inputQueue.front();
+    if (tail->next == nullptr)
+    {
+        // position is assigned by new position.
+        /* YOUR CODE HERE */
+        tail->position = newPosition;
+    }
+    else
+    {
+        SnakeNode *oldTailNode = tail;
+        // cut the old tail off the snake
+        /* YOUR CODE HERE */
+        tail = tail->next;
+        oldTailNode->next = nullptr;
 
-		// remove the front of input queue
-		// YOUR CODE HERE
-		inputQueue.pop();
-
-		// check if snake can move to the next direction, set current direction as next
-		if (canChange(currentDirection, next))
-		{
-			// YOUR CODE HERE
-			currentDirection = next;
-			break;
-		}
-	}
-
-	snake.move(currentDirection);
-}
-
-/***
- * PLEASE REPLACE LINES MARKED WITH '// YOUR CODE HERE'
- *
- * When snake have already eaten a cherry, please add new cherry inside the play screen with random position
- *
- * Args:
- * 		// none
- * Returns:
- * 		// none
- *
- ***/
-
-void Game::addCherry()
-{
-	do
-	{
-		// init a random position inside the play screen (width, height)
-		// Suggestion: use rand() function
-
-		Position randomPos(rand() % width, rand() % height); // YOUR CODE HERE
-
-		// check if the randomPos is EMPTY
-		if (getCellType(randomPos) == CELL_EMPTY)
-		{
-
-			// assign the cherry position as randomPos, and set randomPos type as CELL_CHERRY
-			setCellType(randomPos, CELL_CHERRY);
-			// YOUR CODE HERE
-			// YOUR CODE HERE
-
-			break;
-		}
-	} while (true);
+        // move it to the head of the snake
+        /* YOUR CODE HERE */
+        oldTailNode->position = newPosition;
+        head->next = oldTailNode;
+        head = oldTailNode;
+    }
 }
 
 /***
  * PLEASE UPDATE THIS METHOD
  *
- * set cell of a position as intended type.
- *
+ * When snake eat a Cherry,
+ *  // if snake eat cherry, number of cherries will increase one
  * Args:
- * 		pos (Position): a chosen position
- * 		cellType (CellType): cell type of pos
+ * 		// none
+ *
  * Returns:
  * 		// none
  *
  ***/
-void Game::setCellType(Position pos, CellType cellType)
+void Snake::eatCherry()
 {
-	// if position is inside the play screen (width, height), set to the cellType.
-	// Otherwise, do nothing
-	// Suggestion: use pos.isInsideBox(...) in Position class
-	//
-	// START CODE HERE
-	if (pos.isInsideBox(0, 0, width, height))
-	{
-		squares[pos.y][pos.x] = cellType;
-	}
-	// END CODE HERE
+    /* YOUR CODE HERE */
+    cherry++;
 }
 
-// DO NOT change this method
-CellType Game::getCellType(Position pos) const
+/***
+ * PLEASE UPDATE THIS METHOD
+ *
+ * When snake move in a direction,
+ *  1. Create a newPosition and move:
+ *      Position newPosition = head->position.move(direction);
+ *  2. Using snakeMoveTo method to update game with newPosition
+ *      game.snakeMoveTo(newPosition);
+ *  3.
+ *      If gameOver, return ;
+ *          return;
+ *      elif cherry > 0:
+ *          // If cherry > 0, cherry descrease one and growAtFront() with newPosition
+ *          Note: Call to funtion growAtFront()
+ *      else:
+ *          // SlideTo() newPosition.
+ *          Note: Call to function SlideTo()
+ *
+ * Args:
+ * 		pos (newPosition): head of snake grow at new position
+ *
+ * Returns:
+ * 		// none
+ *
+ ***/
+
+void Snake::move(Direction direction)
 {
-	return pos.isInsideBox(0, 0, width, height) ? squares[pos.y][pos.x] : CELL_OFF_BOARD;
+    Position newPosition = head->position.move(direction);
+
+    /* YOUR CODE HERE */
+    game.snakeMoveTo(newPosition);
+
+    // If gameOver, return ;
+    /* YOUR CODE HERE */
+    if (game.isGameOver())
+        return;
+
+    // If cherry > 0, cherry descrease one and growAtFront() with newPosition
+    if (cherry > 0)
+    {
+        /* YOUR CODE HERE */
+        cherry--;
+        growAtFront(newPosition);
+    }
+    else
+    {
+        game.snakeLeave(tail->position);
+        /* YOUR CODE HERE */
+        slideTo(newPosition);
+    }
 }
 
-// DO NOT change this method
-vector<Position> Game::getSnakePositions() const
+// DO NOT CHANGE METHOD
+int Snake::getNumCherry()
 {
-	return snake.getPositions();
+    return cherry;
 }
 
-// DO NOT change this method
-GameStatus Game::getGameStatus()
+// DO NOT CHANGE METHOD
+SnakeNode *Snake::getHead()
 {
-	return status;
+    return head;
 }
 
-// DO NOT change this method
-int Game::getWidth()
+// DO NOT CHANGE METHOD
+SnakeNode *Snake::getTail()
 {
-	return width;
-}
-
-// DO NOT change this method
-int Game::getHeight()
-{
-	return height;
-}
-
-// DO NOT change this method
-Snake Game::getSnake()
-{
-	return snake;
+    return tail;
 }
